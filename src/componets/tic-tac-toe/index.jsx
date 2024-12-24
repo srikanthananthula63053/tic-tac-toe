c4ba16feef1b6fae8594f416284418a5f99f3ebe
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-24 12:09:49 +0530

    adding useEffect

--- src/componets/tic-tac-toe/index.jsx
+++ src/componets/tic-tac-toe/index.jsx
@@ -1,42 +1,94 @@
-import { useState } from "react";
+// 0 1 2
+// 3 4 5
+// 6 7 8
+
+import { useEffect, useState } from "react";
 import "./styles.css";
 
-// 0,1,2
-// 3,4,5
-// 6,7,8
-
-function Squares({ value, onClick }) {
-    return <button className="Square" onClick={onClick}>{value}</button>
+function Square({ value, onClick }) {
+  return (
+    <button onClick={onClick} className="square">
+      {value}
+    </button>
+  );
 }
 
 export default function TicTacToe() {
-    const [squares, setSquares] = useState(Array(9).fill(''));
-    const [isXturn, setIsXturn] = useState(true)
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
+  const [status, setStatus] = useState("");
 
-    function handleOnClick(getCurrentIndex) {
-        let cpySquares = [...squares]
-        cpySquares[getCurrentIndex] = isXturn ? "X" : "O";
-        setIsXturn(!isXturn);
-        setSquares(cpySquares)
+  function getWinner(squares) {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+    ];
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [x, y, z] = winningPatterns[i];
+
+      if (
+        squares[x] &&
+        squares[x] === squares[y] &&
+        squares[x] === squares[z]
+      ) {
+        return squares[x];
+      }
     }
-    return (
-        <div className="tic-tac-toe-container">
-            <div className="row">
-                <Squares onClick={() => handleOnClick(0)} />
-                <Squares onClick={() => handleOnClick(1)} />
-                <Squares onClick={() => handleOnClick(2)} />
-            </div>
-            <div className="row">
-                <Squares onClick={() => handleOnClick(3)} />
-                <Squares onClick={() => handleOnClick(4)} />
-                <Squares onClick={() => handleOnClick(5)} />
-            </div>
-            <div className="row">
-                <Squares onClick={() => handleOnClick(6)} />
-                <Squares onClick={() => handleOnClick(7)} />
-                <Squares onClick={() => handleOnClick(8)} />
-            </div>
 
-        </div>
-    )
+    return null;
+  }
+
+  function handleClick(getCurrentSquare) {
+    let cpySquares = [...squares];
+    if (getWinner(cpySquares) || cpySquares[getCurrentSquare]) return;
+    cpySquares[getCurrentSquare] = isXTurn ? "X" : "O";
+    setIsXTurn(!isXTurn);
+    setSquares(cpySquares);
+  }
+
+  function handleRestart() {
+    setIsXTurn(true);
+    setSquares(Array(9).fill(""));
+  }
+
+  useEffect(() => {
+    if (!getWinner(squares) && squares.every((item) => item !== "")) {
+      setStatus(`This is a draw ! Please restart the game`);
+    } else if (getWinner(squares)) {
+      setStatus(`Winner is ${getWinner(squares)}. Please restart the game`);
+    } else {
+      setStatus(`Next player is ${isXTurn ? "X" : "O"}`);
+    }
+  }, [squares, isXTurn]);
+
+  console.log(squares);
+
+  return (
+    <div className="tic-tac-toe-container">
+      <div className="row">
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
+      </div>
+      <div className="row">
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
+      </div>
+      <div className="row">
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
+      </div>
+      <h1>{status}</h1>
+      <button onClick={handleRestart}>Restart</button>
+    </div>
+  );
 }
